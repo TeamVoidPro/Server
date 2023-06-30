d1a77b7bb78bb5588c2627162fc1f1e14af97b85
Danodya Supun <89889362+DaNoDya99@users.noreply.github.com>
2023-06-30 23:22:09 +0530

    PasswordHash & JWT Helpers Added

---
+++ Helpers/PasswordHash.cs
@@ -0,0 +1,17 @@
+﻿namespace server.Helpers;
+
+public static class PasswordHash
+{
+    public static string HashPassword(string password)
+    {
+        var salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        
+        return BCrypt.Net.BCrypt.HashPassword(password, salt);
+    }
+
+    public static bool ValidatePassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hash);
+    }
+}
+
---
+++ Helpers/TokenGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace server.Helpers;
+
+public static class TokenGenerator
+{
+    public static string GenerateToken(int id)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, id.ToString())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("This is my custom Secret key for authentication")), SecurityAlgorithms.HmacSha256Signature)
+        };
+        
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        
+        return tokenHandler.WriteToken(token);
+    }
+}
